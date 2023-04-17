ac1e23c5329dfb269c4473396fedbd6de819cc32
MuhammadBurhan235 <muhammadburhan@student.telkomuniversity.ac.id>
2023-04-17 07:30:27 +0700

    FIRST COMMIT

--- jurnalmodul9_1302213109/Controllers/MovieController.cs
+++ jurnalmodul9_1302213109/Controllers/MovieController.cs
@@ -4,24 +4,24 @@ namespace jurnalmodul9_1302213109.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class MovieController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<MovieController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public MovieController(ILogger<MovieController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<jurnalmodul9_1302213109.Movie> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new Movie
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
--- jurnalmodul9_1302213109/Movie.cs
+++ jurnalmodul9_1302213109/Movie.cs
@@ -1,13 +1,10 @@
 namespace jurnalmodul9_1302213109
 {
-    public class WeatherForecast
+    public class Movie
     {
-        public DateTime Date { get; set; }
-
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Description { get; set; }
+        public List<string> Stars { get; set; }
     }
 }
